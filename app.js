c41527b919a772f1bdcdb23a2f4b15fca13c9a8b
ArtemKarlov <ar.karlov@gmail.com>
2021-03-23 19:43:58 +0400

    fix: add class to created input in script

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
+    editInput.classList.add("todo__input");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     // editButton.className="edit";
